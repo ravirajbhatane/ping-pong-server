a1548d0d4e8a487925bb2d2c527904aace230251
Raviraj Bhatane <ravirajbhatane@gmail.com>
2023-04-16 00:59:48 -0700

    Use httprouter library

--- app/ping-pong-server.go
+++ app/ping-pong-server.go
@@ -3,15 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-func ping(w http.ResponseWriter, req *http.Request) {
+func ping(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "text/plain")
 	fmt.Fprintf(w, "pong\n")
 }
 
 func main() {
-	http.HandleFunc("/ping", ping)
+	r := httprouter.New()
+
+	r.GET("/ping", ping)
 
 	fmt.Println("Listening on port 8090")
-	http.ListenAndServe(":8090", nil)
+	http.ListenAndServe(":8090", r)
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ravirajbhatane/ping-pong-server
+
+go 1.20
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
